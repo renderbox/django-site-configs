0cb510efa6aca0dea08f3918ced0838a88e3c5de
Grant Viklund <grant.viklund@gmail.com>
2021-06-29 16:57:58 -0700

    Simplified get_current_site() method

--- siteconfigs/utils.py
+++ siteconfigs/utils.py
@@ -3,8 +3,4 @@ from django.contrib.sites.models import Site
 
 
 def get_current_site(request):
-    if hasattr(request, "site"):
-        current_site = request.site
-    else:
-        current_site = Site.objects.get_current()
-    return current_site
+    return getattr(request, "site", Site.objects.get_current())
