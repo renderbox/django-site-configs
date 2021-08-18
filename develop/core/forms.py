15bf4778a0cfb38634ec45105b4f1926b9f45ecf
mgcamisa <mgcamisa@gmail.com>
2021-08-18 16:36:27 -0700

    remove unnecessary import

--- develop/core/forms.py
+++ develop/core/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 from django.contrib.sites.models import Site
 
-from siteconfigs.models import SiteConfigModel
 from .config import ExampleClass
 
 
